b3e10cccaee9701419bd057b4315ec165ce286b1
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-06-26 07:41:41 +0000

    component linking and ng cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AppNavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl:'./navbar.component.html',
+  // template: ` <h1>INline HTML</h1> `,
+  // styles: `
+  // h1{
+  // color:red;
+  // background:black;
+  // }
+  // `,
+  styleUrl:'./navbar.component.css'
+})
+export class AppNavbarComponent {}
