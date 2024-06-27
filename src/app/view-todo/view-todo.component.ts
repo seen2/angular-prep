2e51717925cf2386575e3bbd59c5a37392e9338b
Lokpati Tiwari <36004515+seen2@users.noreply.github.com>
2024-06-27 04:05:41 +0000

    Routing

---
+++ src/app/view-todo/view-todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-todo.component.html',
+  styleUrl: './view-todo.component.css'
+})
+export class ViewTodoComponent {
+
+}
